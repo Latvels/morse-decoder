44a5224a177dcdebf11b203bb8a3d1a96fd67c54
Evgeny <67653783+Latvels@users.noreply.github.com>
2020-10-20 13:59:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,8 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+
+    let result = "";
+
+    while(expr.length >= 10) {
+
+        let sliceString = expr.slice(0, 10);
+        let keyObject = "";
+
+        for (let i = 0; i <= 8; i += 2  ){            
+            if (sliceString[i] === "*"){
+                keyObject += " ";
+                i = 9;
+            } else if (sliceString[i] === "0"){
+                keyObject += "";
+            } else if (sliceString[i] === "1" && sliceString[i + 1] === "0"){
+                keyObject += "."
+            } else if (sliceString[i] === "1" && sliceString[i + 1] === "1"){
+                keyObject += "-"
+            }
+            }
+
+            if (keyObject !== " "){
+                result += MORSE_TABLE[keyObject];
+            } else {
+                result += " ";
+            }
+
+            expr = expr.slice(10);
+        }
+
+        return result;
+    }
 
 module.exports = {
     decode
